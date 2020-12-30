f5f4f41fc989fc054f2d74270e3b4e03a3042a54
tcastanie <thibaut.castanie@gmail.com>
2020-12-30 12:23:40 +0100

    connection bdd

--- API/index.js
+++ API/index.js
@@ -1,31 +1,49 @@
 const express = require('express');
 const app = express();
 
-const events = require('./test/events.json');
-const tags = require('./test/tags.json');
+require('dotenv').config();
 
-app.get('/events', async (req,res) => {
+// const events = require('./test/events.json');
+// const tags = require('./test/tags.json');
+
+const HOST = process.env.DB_HOST;
+const USER = process.env.DB_USER;
+const PASS = process.env.DB_PASS;
+const DBNAME = process.env.DB_NAME;
+const PORT = process.env.PORT || 2020;
+const uri = `mongodb+srv://${USER}:${PASS}@${HOST}/${DBNAME}?retryWrites=true&w=majority`;
+
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+let db;
+client.connect(err => {
+    //console.log("Connected successfully to server");
+    db = client.db(DBNAME);
+    //client.close();
+});
+
+app.use(express.json());
+
+app.get('/events', async (req, res) => {
     try {
-        //const docs = await db.collection('events').find({}).toArray()
-        //res.status(200).json(docs)
-        res.status(200).json(events)
+        const docs = await db.collection('events').find({}).toArray();
+        res.status(200).json(docs);
     } catch (err) {
-        console.log(err)
-        throw err
+        console.log(err);
+        throw err;
     }
 })
 
-app.get('/tags', async (req,res) => {
+app.get('/tags', async (req, res) => {
     try {
-        //const docs = await db.collection('events').find({}).toArray()
-        //res.status(200).json(docs)
-        res.status(200).json(tags)
+        const docs = await db.collection('tags').find({}).toArray();
+        res.status(200).json(docs);
     } catch (err) {
-        console.log(err)
-        throw err
+        console.log(err);
+        throw err;
     }
 })
 
-app.listen(2020, () => {
-    console.log('Tomate-planning server running on port 2020')
+app.listen(PORT, () => {
+    console.log(`Tomate-planning server running on port ${PORT}`);
 })
