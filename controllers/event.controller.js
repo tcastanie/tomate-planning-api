0b4979ca1b97edff4d2be517bc6dc51df60348d1
tcastanie <thibaut.castanie@gmail.com>
2021-01-23 18:48:23 +0100

    fix deprecation mongoose

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -92,7 +92,7 @@ exports.update = async (req, res) => {
     }
     const id = req.params.id;
     try {
-        const data = await Event.findByIdAndUpdate(id, req.body, { new: true });
+        const data = await Event.findByIdAndUpdate(id, req.body, { new: true, useFindAndModify: false });
         if (!data) {
             res.status(404).send({ message: `Cannot update Event with id=${id}. Maybe Event was not found` });
         } else {
