86f7f15d1f56b5a6cf92bd037036ba99b4eccfa1
tcastanie <thibaut.castanie@gmail.com>
2021-01-24 14:10:43 +0100

    routes Tags done

--- controllers/tag.controller.js
+++ controllers/tag.controller.js
@@ -1,14 +1,31 @@
 const db = require("../models");
 const Tag = db.tags;
 
-//tags
-/*
-GET /tags
-GET /tags/:id
-POST /tags
-PUT /tags/:id
-DELETE /tags/:id
-*/
+// Create and save a new Tag
+exports.create = async (req, res) => {
+    if (!req.body.title || !req.body.color) {
+        res.status(400).send({ message: "New Tag need a title and color" });
+        return;
+    }
+    const tag = new Tag({
+        title: req.body.title,
+        color: req.body.color,
+        desc: req.body.desc,
+    });
+    try {
+        const data = await tag.save(tag);
+        res.status(201).send({
+            message: "New Tag created",
+            data: data
+        });
+    } catch (err) {
+        res.status(500).send({
+            message: err.message,
+            desc: "Error creating new Tag"
+        });
+        throw err;
+    }
+};
 
 // Retrieve all Tags from the database.
 exports.findAll = async (req, res) => {
@@ -16,7 +33,77 @@ exports.findAll = async (req, res) => {
         const docs = await Tag.find({});
         res.status(200).send(docs);
     } catch (err) {
-        res.status(500).send({ message: err.message || "Error while retrieving all Tags" });
+        res.status(500).send({
+            message: err.message,
+            desc: "Error while retrieving all Tags"
+        });
+        throw err;
+    }
+};
+
+// Retrieve a single Tag with id
+exports.findOne = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const docs = await Tag.findById(id);
+        if (!docs) {
+            res.status(404).send({ message: "Not found Tag with id " + id });
+        } else {
+            res.status(200).send(docs);
+        }
+    } catch (err) {
+        res.status(500).send({
+            message: err.message,
+            desc: "Error retrieving Tag with id " + id
+        });
+        throw err;
+    }
+};
+
+// Update a Tag with id
+exports.update = async (req, res) => {
+    if (!req.body) {
+        res.status(400).send({ message: "Data to update can not be empty" });
+        return;
+    }
+    const id = req.params.id;
+    try {
+        const data = await Tag.findByIdAndUpdate(id, req.body, { new: true, useFindAndModify: false });
+        if (!data) {
+            res.status(404).send({ message: `Cannot update Tag with id=${id}. Maybe Tag was not found` });
+        } else {
+            res.status(200).send({
+                message: "Tag updated successfully",
+                data: data
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            message: err.message,
+            desc: "Error updating Tag with id " + id
+        });
+        throw err;
+    }
+};
+
+// Delete a Tag with id
+exports.delete = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const data = await Tag.findByIdAndRemove(id);
+        if (!data) {
+            res.status(404).send({ message: `Cannot delete Tag with id=${id}. Maybe Tag was not found` });
+        } else {
+            res.status(200).send({
+                message: "Tag deleted successfully",
+                data: data
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            message: err.message,
+            desc: "Could not remove Tag with id " + id
+        });
         throw err;
     }
 };
--- routes/tag.routes.js
+++ routes/tag.routes.js
@@ -2,8 +2,20 @@ module.exports = app => {
     const tags = require("../controllers/tag.controller.js");
     var router = require("express").Router();
 
+    // Create a new Tag
+    router.post("/", tags.create);
+
     // Retrieve all Tags
     router.get("/", tags.findAll);
+
+    // Retrieve a single Tag with id
+    router.get("/:id", tags.findOne);
+
+    // Update a Tag with id
+    router.put("/:id", tags.update);
+
+    // Delete a Tag with id
+    router.delete("/:id", tags.delete);
 
     app.use('/tags', router);
 };
